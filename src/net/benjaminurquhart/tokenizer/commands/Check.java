ecdaebdbb66f52941ac484f861b6bc5feeaefdb1
BuildTools <unconfigured@null.spigotmc.org>
2019-01-16 18:01:41 -0500

    Added queue

--- src/net/benjaminurquhart/tokenizer/commands/Check.java
+++ src/net/benjaminurquhart/tokenizer/commands/Check.java
@@ -2,8 +2,14 @@ package net.benjaminurquhart.tokenizer.commands;
 
 import java.awt.Color;
 import java.io.IOException;
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import org.kohsuke.github.GHContent;
 import org.kohsuke.github.GitHub;
@@ -13,16 +19,32 @@ import net.benjaminurquhart.jch.Command;
 import net.benjaminurquhart.tokenizer.Tokenizer;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 
 public class Check extends Command<Tokenizer> {
-
+	
 	private GitHub github;
+	
+	private ScheduledExecutorService executor;
+	
+	protected List<QueuedAction> queue;
 	
 	public Check(){
 		super("check","bot id");
+		executor = Executors.newSingleThreadScheduledExecutor();
+		queue = Collections.synchronizedList(new ArrayList<>());
+		executor.scheduleWithFixedDelay(() -> {
+			try {
+				if(!queue.isEmpty()) {
+					queue.remove(0).run();
+				}
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+			}
+		}, 0, 5, TimeUnit.SECONDS);
 	}
-	@SuppressWarnings("deprecation")
 	@Override
 	public void handle(GuildMessageReceivedEvent event, Tokenizer self) {
 		TextChannel channel = event.getChannel();
@@ -41,28 +63,7 @@ public class Check extends Command<Tokenizer> {
 				}
 			}
 			event.getJDA().retrieveUserById(args[2]).queue((user) -> {
-				try{
-					channel.sendTyping().queue();
-					String b64ID = Base64.getEncoder().encodeToString(args[2].getBytes());
-					List<GHContent> results = github.searchContent().q(b64ID).list().asList();
-					for(GHContent result : results){
-						if(result.getContent().contains(b64ID)){
-							EmbedBuilder eb = new EmbedBuilder();
-							eb.setColor(Color.RED);
-							eb.setTitle("Token Found", result.getGitUrl());
-							eb.setDescription(user.getAsMention());
-							eb.addField("Repository", String.format("[%s](%s)", result.getOwner().getName(), result.getOwner().getHttpTransportUrl()), true);
-							eb.addField("File", String.format("[%s](%s)", result.getName(), result.getHtmlUrl()), true);
-							channel.sendMessage(eb.build()).queue();
-							return;
-						}
-					}
-					channel.sendMessage("No results found! Safe!").queue();
-				}
-				catch(IOException e){
-					channel.sendMessage(e.toString()).queue();
-					e.printStackTrace();
-				}
+				channel.sendMessage(String.format("Your request has been queued (Approximate wait time: %d seconds)", queue.size()*5)).queue((m) -> queue.add(new QueuedAction(event, user, github)));
 			}, (e) -> channel.sendMessage(e.toString()).queue());
 		}
 		catch(Exception e){
@@ -70,5 +71,47 @@ public class Check extends Command<Tokenizer> {
 			e.printStackTrace();
 		}
 	}
-
+}
+final class QueuedAction implements Runnable{
+	
+	private GuildMessageReceivedEvent event;
+	private GitHub github;
+	private User user;
+	
+	protected QueuedAction(GuildMessageReceivedEvent event, User user, GitHub github) {
+		this.github = github;
+		this.event = event;
+		this.user = user;
+	}
+	@SuppressWarnings("deprecation")
+	@Override
+	public void run() {
+		TextChannel channel = event.getChannel();
+		try{
+			channel.sendTyping().queue();
+			User author = event.getAuthor();
+			String b64ID = Base64.getEncoder().encodeToString(user.getId().getBytes());
+			List<GHContent> results = github.searchContent().q(b64ID).list().asList();
+			for(GHContent result : results){
+				if(result.getContent().contains(b64ID)){
+					EmbedBuilder eb = new EmbedBuilder();
+					eb.setColor(Color.RED);
+					eb.setTimestamp(Instant.now());
+					eb.setTitle("Token Found", result.getGitUrl());
+					eb.setDescription(user.getAsMention());
+					eb.addField("Repository", String.format("[%s](%s)", result.getOwner().getName(), result.getOwner().getHtmlUrl()), true);
+					eb.addField("File", String.format("[%s](%s)", result.getName(), result.getHtmlUrl()), true);
+					eb.setFooter(String.format("Requested by %s#%s", author.getName(), author.getDiscriminator()), author.getEffectiveAvatarUrl());
+					channel.sendMessage(author.getAsMention()).embed(eb.build()).queue();
+					return;
+				}
+			}
+			channel.sendMessage(author.getAsMention() + " No results found! Safe!").queue();
+		}
+		catch(Exception e){
+			channel.sendMessage(e.toString()).queue();
+			e.printStackTrace();
+		}
+	}
+	
 }
